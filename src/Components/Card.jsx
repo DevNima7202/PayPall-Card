daa0f77513ea1e11342a5c89935415f8340c3a99
Nima soori <nimasoori27@gmail.com>
2023-04-16 23:30:34 +0430

    Card Component aded

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,7 +5,7 @@ function Card() {
   return (
     <div className=' w-1/2 h-full'>
         <div className='w-[80%] h-52 mt-[70px] ml-10  bg rounded-xl border border-gray-500'>
-        
+        <div></div>
         </div>
     </div>
   )
